86db27fd89fe31858d51f2c79a0be85cdb789534
scress <scottcresswell@earthlink.net>
2016-01-13 19:26:21 -0800

    First commit

---
+++ guess.js
@@ -0,0 +1,25 @@
+
+
+var guess = prompt("Did I go to college");
+if (guess ="yes" || "y"||"Y"||"YES"){
+	alert ("I did go to college!");
+	writeImage()
+
+}
+else if (guess="no"||"n"||"N"||"NO"){
+alert ("Try again");
+
+}
+var guess = prompt("Am in from Washington?");
+if (guess="yes"||"y"||"Y"||"YES"){
+	alert("That is correct");
+	writeImage()
+}
+else if (guess="no"||"n"||"N"||"NO"){
+alert("Sorry wrong answer!");
+}
+var guess = prompt ("Am i a niners fan?");
+if (guess="no"||"n"||"N"||"NO") {
+	alert("Thats right! I'm a Seahawks fan");
+	writeImage()
+}
